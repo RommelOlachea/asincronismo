227fe545672b50054c60f23dcdb3c6690752f0e4
RommelOlachea <msc.rommel.olachea@gmail.com>
2022-03-10 14:00:51 -0700

    Finalizo callbacks-cecc

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -23,17 +23,14 @@ function fetchData(url_api, callback){
 }
               //aqui esta definida, la funcion de callback, en el llamado de fetchData
 fetchData(API,function(error1,data1){
-    console.log(API);
+    //console.log(API);
     if(error1) return console.log(error1); //validamos que la funcion no nos haya dado un error.       
     fetchData(API + data1.results[0].id, function(error2, data2){
-        console.log(API + data1.results[0].id);
-        if(error2) return console.log(error2);
-   
-        
+        //console.log(API + data1.results[0].id);
+        if(error2) return console.log(error2);           
         fetchData(data2.origin.url, function(error3,data3){   
-            console.log(data2.origin.url);
+            //console.log(data2.origin.url);
             if(error3) return console.log(error3);
-
             console.log(data1.info.count);
             console.log(data2.name);
             console.log(data3.dimension);
